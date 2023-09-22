01d0b468324a3bc5d5bbe80ce39f613a55998e05
Gus Narea <gnarea@users.noreply.github.com>
2023-09-22 20:19:00 +0100

    Upgrade relaycorp/veraid-authority/google to v1.4.1

--- authority.tf
+++ authority.tf
@@ -5,7 +5,7 @@ locals {
 
 module "authority" {
   source  = "relaycorp/veraid-authority/google"
-  version = "1.4.0"
+  version = "1.4.1"
 
   instance_name = local.instance_name
 
@@ -20,7 +20,7 @@ module "authority" {
   mongodb_password = random_password.mongodb_authority_user_password.result
 
   api_auth_audiences = [var.veraid_authority_api_auth_audience, local.authority_api_auth_audience]
-  superadmin_sub     = var.superadmin_email_address
+  superadmin_email   = var.superadmin_email_address
 
   kms_protection_level = "HSM"
 
